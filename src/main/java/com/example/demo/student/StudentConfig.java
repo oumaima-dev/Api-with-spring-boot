fe7f14cf51e27954ee5c08bcd6410f2a1554a409
pc <oumaima.kziber@uit.ac.ma>
2021-04-20 22:58:02 +0200

    update

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+          Student maria = new Student("maria", 22,
+                  LocalDate.of(1998, Month.AUGUST,17),
+                  " maria.la@gmail.com");
+
+            Student laith = new Student("laith", 22,
+                    LocalDate.of(1991, Month.JANUARY,1),
+                    " laith@gmail.com");
+
+            studentRepository.saveAll(List.of(maria,laith));
+
+        };
+    }
+}
