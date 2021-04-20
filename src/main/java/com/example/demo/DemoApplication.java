44ff3ee25617c12ee1e6422cc172ce589bd379a1
pc <oumaima.kziber@uit.ac.ma>
2021-04-20 15:10:33 +0200

    add service+controller

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,10 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
@@ -19,11 +12,4 @@ public class DemoApplication {
 	}
 
 
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(new Student(1L, "oumaima", 22,
-				LocalDate.of(1998, Month.AUGUST,17),
-				" oum@gmail.com"));
-
-	}
 }
